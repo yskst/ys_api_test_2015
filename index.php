1086b5126cb079a316d5522a9356302a6ac37302
you <you@localhost.localdomain>
2015-02-07 16:17:56 +0900

    index.php: Add submit button which send to itself. index.php: After search button pushed, the condition of search is saved.

--- index.php
+++ index.php
@@ -7,19 +7,24 @@
 <body>
 <h1>The test of yahoo API.</h1>
 
-<input type="text" name="query"><br>
-
+<form action=<?php echo $_SERVER['PHP_SELF']?> method="post">
 <?php
+
+echo "<input type=\"text\" value=$query name=\"query\"><br>"
+
 require_once('./category_selector.php');
 $top_category = get_child_category_list();
 
 echo '<select name="category_id">';
 foreach ($top_category as $category){
-  echo "<option value=\"$category->Id\">". $category->Title->Short ."</option>";
+  if ($category_id == $category->Id) $s="selected";
+  else $s = "";
+  echo "<option value=\"$category->Id\" $selected>". $category->Title->Short ."</option>";
 }
 echo "</select>";
-
 ?>
+<input type="submit" value="検索">
+</form>
 
 </body>
 </html>
