2208400c19edb125f096d1fef232c24ac66ea0c6
yskst <pwmtagmj@yahoo.co.jp>
2015-02-07 11:26:20 +0900

    Add Text area to input query.

--- index.php
+++ index.php
@@ -7,16 +7,15 @@
 <body>
 <h1>The test of yahoo API.</h1>
 
-<?php
-ini_set('display_errors', 1);
+<input type="text" name="query"><br>
 
+<?php
 require_once('./category_selector.php');
 $top_category = get_child_category_list();
 
 echo '<select name="category_id">';
 foreach ($top_category as $category){
-  var_dump($category);
-  echo "<option value=\"$category->Id\">". $category->Title->Short." </option>";
+  echo "<option value=\"$category->Id\">". $category->Title->Short ."</option>";
 }
 echo "</select>";
 
