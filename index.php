9e8a4464bf4d1f5d6446fd82d0aacad17677fba3
root <root@localhost.localdomain>
2015-02-09 00:00:25 +0900

    The data send method change to "get".

--- index.php
+++ index.php
@@ -7,17 +7,21 @@
 <body>
 <h1>The test of yahoo API.</h1>
 
-<form action=<?php echo $_SERVER['PHP_SELF']?> method="post">
+<form action=<?php echo $_SERVER['PHP_SELF']?> method="get">
 <?php
 
-echo "<input type=\"text\" value=\"". $_POST["query"] ."\"name=\"query\"><br>\n";
+echo "<input type=\"text\" value=\"". $_GET["query"] ."\"name=\"query\"><br>\n";
 
 require_once('./category_selector.php');
 $top_category = get_child_category_list();
 
 echo '<select name="category_id">';
+
+if(isset($_GET["category_id"])) $s = "selected";
+else $s="";
+echo '<option value=1>すべてのカテゴリ</option>';
 foreach ($top_category as $category){
-  if ($_POST["category_id"] == intval($category->Id)) $s="selected\n";
+  if ($_GET["category_id"] == intval($category->Id)) $s="selected";
   else $s = "";
   echo "<option value=\"$category->Id\" $s>". $category->Title->Short ."</option>\n";
 }
