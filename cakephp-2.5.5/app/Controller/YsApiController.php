39b898287106f6bab6b7d5e33024b6785ca182f8
you <you@localhost.localdomain>
2015-02-12 01:38:28 +0900

    Implement functions which used in the serching.

--- cakephp-2.5.5/app/Controller/YsApiController.php
+++ cakephp-2.5.5/app/Controller/YsApiController.php
@@ -11,9 +11,31 @@ class YsApiController extends AppController{
     $xml = simplexml_load_file($url);
     $category_list = array();
     foreach($xml->Result->Categories->Children->Child as $category){
-      $category_list += array((int)$category->Id => $category->Title->Short);
+      $category_list += array((int)$category->Id =>$category->Title->Short);
     }
     return $category_list;
+  }
+
+  private function __GetRank($sortid){
+    $url = "http://shopping.yahooapis.jp/ShoppingWebService/V1/itemSearch";
+    $c = rawurlencode($category_id);
+    $s = rawurlencode($sortid);
+    $url = "$url?appid=$this->appid&query=$query&category_id=$c&sort=$s&hits=5";
+    $xml = simplexml_load_file($url);
+    return $xml; 
+  }
+
+  private function sort2name($sotr_type){
+    if($sort_type == "price") return "価格";
+    else if($sort_type == "score") return "評価";
+    else if($sort_type == "sold" ) return "売れ筋";
+    else if($sort_type == "review_count") return "評価";
+    else return "その他";
+  }
+
+  private function order2name($order_type){
+    if($order_type == "+") return "昇順";
+    else return "降順";
   }
 
   public function index(){
@@ -22,6 +44,8 @@ class YsApiController extends AppController{
   
     $category_id = $this->__GetCategory();
     $this->set("category_list", $category_id);
+
+    // Get result of search.
   }
 }
 ?>
