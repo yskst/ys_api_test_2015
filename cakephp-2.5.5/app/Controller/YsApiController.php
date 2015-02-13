03f7ece7d04267925f9b8b5878f101bd304fc63f
yskst <pwmtagmj@yahoo.co.jp>
2015-02-14 01:13:45 +0900

    Implements the part of showing Result.

--- cakephp-2.5.5/app/Controller/YsApiController.php
+++ cakephp-2.5.5/app/Controller/YsApiController.php
@@ -1,6 +1,7 @@
 <?php
 App::uses('AppController', 'Controller');
 App::uses('Sanitize', 'Utility');
+App::uses('Xml', 'Utility');
 
 class YsApiController extends AppController{
   private $appid="dj0zaiZpPU1NOEE5dG9VcjBFSSZzPWNvbnN1bWVyc2VjcmV0Jng9NmE-";
@@ -9,53 +10,60 @@ class YsApiController extends AppController{
 
     $url = "http://shopping.yahooapis.jp/ShoppingWebService/V1/categorySearch?appid=$this->appid&category_id=$category_id";
     $xml = simplexml_load_file($url);
-    $category_list = array();
+    $category_list = array(1 => "すべてのカテゴリ");
     foreach($xml->Result->Categories->Children->Child as $category){
       $category_list += array((int)$category->Id =>$category->Title->Short);
     }
     return $category_list;
   }
 
-  private function __GetRank($query, $category_id, $sortid){
+   private function __GetSortArray(){
+     $sorts = array("price" => "価格",
+                    "score" => "評価",
+                    "sold"  => "売れ筋",
+                    "review_count" => "評価"); 
+     $orders = array("+" => "昇順",
+                     "-" =>  "降順");
+     $a = array();
+     foreach($orders as $oid=>$oname){
+       foreach($sorts as $sid=>$sname){
+         $a += array($oid.$sid => $sname.$oname);
+        }
+     }
+     return $a;
+   }
+
+  private function search($query, $category, $sort_word){
     $url = "http://shopping.yahooapis.jp/ShoppingWebService/V1/itemSearch";
-    $c = rawurlencode($category_id);
-    $s = rawurlencode($sortid);
+    $c = rawurlencode($category);
+    $s = rawurlencode($sort_word);
+
     $url = "$url?appid=$this->appid&query=$query&category_id=$c&sort=$s&hits=5";
-    $xml = simplexml_load_file($url);
-    return $xml; 
-  }
-
-  private $sorts = array("price" => "価格",
-                        "score" => "評価",
-                        "sold"  => "売れ筋",
-                        "review_count" => "評価"); 
-  private $orders = array("+" => "昇順",
-                          "-" =>  "降順");
-
-  private function CreateSortParam($sort, $order){
-    if(!array_key_exists($sort,$sorts) || 
-        !array_key_exists($order,$orders)){
-      throw BadRequestException;
-    }
-    return $order.$sort;
+    $xml = Xml::toArray(Xml::build($url));
+    $xml = $xml["ResultSet"];
+    if($xml["@totalResultsReturned"] == 0) throw BadRequestException;
+    return $xml;
   }
 
 
   public function index(){
+    // Relate with search window.
     $this->modelClass = null;
     $this->set("title_for_layout", "Test of Yahoo API");
-  
+
     $category_id = $this->__GetCategory();
     $this->set("category_list", $category_id);
 
-    // Get result of search.
-    if(isset($this->data["query"]) && isset($this->data["category"])){
-      foreach($orders as $otype => $oname){
-        foreach($sorts as $stype => $sname){
-          $sort_type = CreateSortParam($stype, $otype);
-        }
-      }
+    $sort_list=$this->__GetSortArray();
+    $this->set("sort_list", $sort_list);
+
+    // Searching.
+    if(isset($this->data["query"])){
+      $res = $this->search($this->data["query"], 
+        $this->data["category"], $this->data["sort"]);
+      $this->set("results", $res["Result"]["Hit"]);   
     }
   }
 }
 ?>
+
--- cakephp-2.5.5/app/View/YsApi/index.ctp
+++ cakephp-2.5.5/app/View/YsApi/index.ctp
@@ -4,6 +4,24 @@ echo $this->Form->create(false, array('type'=>'post', 'action'=>'.'));
 echo $this->Form->label('query', "クエリ");
 echo $this->Form->text('query');
 echo $this->Form->label('category', "カテゴリ");
-echo $this->Form->select('category', $category_list);
+echo $this->Form->select('category', $category_list, array('empty'=>array_slice($category_list, 1, 1, true)));
+echo $this->Form->label('sort', "並び順");
+echo $this->Form->select('sort', $sort_list, array('empty'=>array_slice($sort_list, 1, 1, true)));
 echo $this->form->end("検索");
 ?>
+
+<h1>検索結果</h1>
+<?php 
+if(isset($results)){
+  foreach($results as $res){ 
+    echo "<p>";
+    echo $this->Html->link(
+      $this->Html->image($res["Image"]["Small"],array('fullBase'=>true)),
+      $res["Url"], array('escape'=>false));
+    echo $this->Html->link($res["Name"]."<br>".$res["Store"]["Name"],
+                               $res["Url"], array('escape'=>false));
+    echo "</p>";
+  }
+}
+?>
+
